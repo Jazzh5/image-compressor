6ea161317f8b1267665cdefe5c64e18d2151fdd6
曹玉 <you.cao@marketingforce.com>
2026-05-22 14:04:07 +0800

    feat: report

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+import { inject } from '@vercel/analytics'
 import './style.css'
 import App from './App.vue'
+
+inject({ mode: 'auto' });
 
 const app = createApp(App)
 app.mount('#app')
